473ff37f9871c95f8167777fb8ed34e59b2519f8
Maria Tereshina <mtereshina0509@yandex.ru>
2021-11-07 19:02:29 +0300

    added docstings and reformat variable names to upper-case

--- homework00/hello.py
+++ homework00/hello.py
@@ -1,7 +1,11 @@
+"""This script creates a greeting message"""
+
+
 def get_greeting(name: str) -> str:
+    """Returns greeting"""
     return "Hello, " + name + "!"
 
 
 if __name__ == "__main__":
-    message = get_greeting("World")
-    print(message)
+    MESSAGE = get_greeting("World")
+    print(MESSAGE)
