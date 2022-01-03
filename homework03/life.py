4f366c13401d66f8faf207487a5320e729d46132
Maria Tereshina <90193210+maria-tereshina-336999@users.noreply.github.com>
2022-01-03 17:04:04 +0300

    Add files via upload

--- homework03/life.py
+++ homework03/life.py
@@ -1,8 +1,6 @@
 import pathlib
 import random
 import typing as tp
-from random import randint
-from copy import copy, deepcopy
 
 import pygame
 from pygame.locals import *
@@ -32,55 +30,20 @@ class GameOfLife:
 
     def create_grid(self, randomize: bool = False) -> Grid:
         # Copy from previous assignment
-        grid = []
-        for i in range(self.rows):
-            grid.append([])
-            for j in range(self.cols):
-                if randomize == True:
-                    znach = random.randint(0, 1)
-                else:
-                    znach = 0
-                grid[i].append(znach)
-        return grid
+        pass
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        x1 = cell[0]
-        y1 = cell[1]
-
-        mas = []
-        for i in range(x1 - 1, x1 + 2):
-            for j in range(y1 - 1, y1 + 2):
-                if 0 <= i <= self.rows - 1 and 0 <= j <= self.cols - 1:
-                    if x1 != i or y1 != j:
-                        mas.append(self.curr_generation[i][j])
-
-        return mas
-
+        # Copy from previous assignment
         pass
 
     def get_next_generation(self) -> Grid:
-        grid_copy = deepcopy(self.curr_generation)
-        for i in range(len(self.curr_generation)):
-            for j in range(len(self.curr_generation[0])):
-                k=(i,j)
-                l=sum(self.get_neighbours(k))
-                if l!=2 and l!=3 and self.curr_generation[i][j]==1:
-                    grid_copy[i][j]=0
-                elif l==3 and self.curr_generation[i][j]==0:
-                    grid_copy[i][j]=1
-        return grid_copy
+        # Copy from previous assignment
         pass
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        self.prev_generation=deepcopy(self.curr_generation)
-        self.curr_generation=self.get_next_generation()
-        self.generations=self.generations+1
-
-
-
         pass
 
     @property
@@ -88,7 +51,6 @@ class GameOfLife:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        return self.generations<=self.max_generations
         pass
 
     @property
@@ -96,7 +58,6 @@ class GameOfLife:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        return self.prev_generation!=self.curr_generation
         pass
 
     @staticmethod
@@ -104,21 +65,10 @@ class GameOfLife:
         """
         Прочитать состояние клеток из указанного файла.
         """
-
-        with open(filename, "r") as file:
-            mas1 = [i.split() for i in file]
-            mas2 = [list(str(mas1[i][0])) for i in range(len(mas1))]
-            mas3 = [[int(j) for j in mas2[i]] for i in range(len(mas2))]
-            return (mas3)
         pass
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        file = open(filename, "w")
-        l = ["".join(map(str, self.curr_generation[i])) for i in range(len(self.curr_generation))]
-        k = "\n".join(l)
-        file.write(str(k))
-        file.close()
         pass
--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -7,77 +7,15 @@ from ui import UI
 class GUI(UI):
     def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
-        self.cell_size=cell_size
-        self.speed=speed
-        self.screen_size = (self.life.rows * self.cell_size,  self.life.cols * self.cell_size )
-        self.screen = pygame.display.set_mode(self.screen_size)
-
 
     def draw_lines(self) -> None:
-        for x in range(0, self.screen_size[1], self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.screen_size[0]))
-        for y in range(0, self.screen_size[0], self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.screen_size[1], y))
+        # Copy from previous assignment
         pass
 
     def draw_grid(self) -> None:
-        x=0
-        y=self.cell_size*(-1)
-        grid=self.life.curr_generation
-        for i in range(self.life.rows):
-            y=y+self.cell_size
-            x=0
-            for j in range(self.life.cols):
-                if grid[i][j]==1:
-                    pygame.draw.rect(self.screen, pygame.Color('green'),(x,y,self.cell_size,self.cell_size)  )
-                    x=x+self.cell_size
-                else:
-                    pygame.draw.rect(self.screen, pygame.Color('white'),(x,y,self.cell_size,self.cell_size ) )
-                    x=x+self.cell_size
-
-
-
+        # Copy from previous assignment
         pass
 
     def run(self) -> None:
-        pygame.init()
-        clock = pygame.time.Clock()
-        pygame.display.set_caption("Game of Life")
-        self.screen.fill(pygame.Color("white"))
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    running = False
-                if event.type==pygame.KEYDOWN:
-                    if event.key==pygame.K_ESCAPE:
-                        pygame.quit()
-                    if event.key==pygame.K_SPACE:
-                        while True:
-                            event = pygame.event.wait()
-                            if event.type == pygame.KEYDOWN:
-                                if event.key == pygame.K_SPACE:
-                                    break
-                            if event.type==pygame.MOUSEBUTTONDOWN:
-                                if event.button==1:
-                                    x=event.pos[0]//self.cell_size
-                                    y=event.pos[1]//self.cell_size
-                                    self.life.curr_generation[y][x]=1
-                                    self.draw_grid()
-                                    self.draw_lines()
-                                    pygame.display.flip()
-            self.life.step()
-            self.draw_grid()
-            self.draw_lines()
-            pygame.display.flip()
-            clock.tick(self.speed)
-
-        pygame.quit()
-
-
+        # Copy from previous assignment
         pass
-
-
-life = GameOfLife((30, 30))
-ui = GUI(life)
-ui.run()
