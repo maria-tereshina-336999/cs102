5b341e4a0ebaa340006dd43b342d0e9219ce9d93
Maria Tereshina <90193210+maria-tereshina-336999@users.noreply.github.com>
2022-05-17 23:39:00 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,5 +1,4 @@
-from typing import Any
-
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
