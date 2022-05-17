8eb47805b29bbf8aa431eb6e21669df2c8f00b0e
Maria Tereshina <90193210+maria-tereshina-336999@users.noreply.github.com>
2022-05-17 22:11:50 +0300

    black, isort

--- practice_frameworks/todo.py
+++ practice_frameworks/todo.py
@@ -1,40 +1,50 @@
 import sqlite3
-from bottle import route, run, debug, template, request, static_file, error, redirect
 
 # only needed when you run Bottle on mod_wsgi
-from bottle import default_app
+from bottle import (
+    debug,
+    default_app,
+    error,
+    redirect,
+    request,
+    route,
+    run,
+    static_file,
+    template,
+)
 
 
-@route('/todo')
+@route("/todo")
 def todo_list():
 
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect("todo.db")
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '1'")
     result = c.fetchall()
     c.close()
 
-    output = template('make_table', rows=result)
+    output = template("make_table", rows=result)
     return output
 
-@route('/deleted')
+
+@route("/deleted")
 def deleted_list():
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect("todo.db")
     c = conn.cursor()
     c.execute("SELECT id, task FROM todo WHERE status LIKE '0'")
     result = c.fetchall()
     c.close()
-    output = template('make_table_for_del', rows=result)
+    output = template("make_table_for_del", rows=result)
     return output
 
 
-@route('/new', method='GET')
+@route("/new", method="GET")
 def new_item():
 
     if request.GET.save:
 
         new = request.GET.task.strip()
-        conn = sqlite3.connect('todo.db')
+        conn = sqlite3.connect("todo.db")
         c = conn.cursor()
 
         c.execute("INSERT INTO todo (task,status) VALUES (?,?)", (new, 1))
@@ -43,82 +53,84 @@ def new_item():
         conn.commit()
         c.close()
 
-        redirect('/todo')
+        redirect("/todo")
 
     else:
-        return template('new_task.tpl')
+        return template("new_task.tpl")
 
 
-@route('/edit/<no:int>', method='GET')
+@route("/edit/<no:int>", method="GET")
 def edit_item(no):
 
     if request.GET.save:
         edit = request.GET.task.strip()
         status = request.GET.status.strip()
 
-        if status == 'Сохранить':
+        if status == "Сохранить":
             status = 1
         else:
             status = 0
 
-        conn = sqlite3.connect('todo.db')
+        conn = sqlite3.connect("todo.db")
         c = conn.cursor()
         c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
         conn.commit()
 
-        redirect('/todo')
+        redirect("/todo")
     else:
-        conn = sqlite3.connect('todo.db')
+        conn = sqlite3.connect("todo.db")
         c = conn.cursor()
         c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no)))
         cur_data = c.fetchone()
 
-        return template('edit_task', old=cur_data, no=no)
+        return template("edit_task", old=cur_data, no=no)
 
 
-@route('/item<item:re:[0-9]+>')
+@route("/item<item:re:[0-9]+>")
 def show_item(item):
 
-        conn = sqlite3.connect('todo.db')
-        c = conn.cursor()
-        c.execute("SELECT task FROM todo WHERE id LIKE ?", (item,))
-        result = c.fetchall()
-        c.close()
+    conn = sqlite3.connect("todo.db")
+    c = conn.cursor()
+    c.execute("SELECT task FROM todo WHERE id LIKE ?", (item,))
+    result = c.fetchall()
+    c.close()
 
-        if not result:
-            return 'This item number does not exist!'
-        else:
-            return 'Task: %s' % result[0]
+    if not result:
+        return "This item number does not exist!"
+    else:
+        return "Task: %s" % result[0]
 
 
-@route('/help')
+@route("/help")
 def help():
 
-    static_file('help.html', root='.')
+    static_file("help.html", root=".")
 
 
-@route('/json<json:re:[0-9]+>')
+@route("/json<json:re:[0-9]+>")
 def show_json(json):
 
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect("todo.db")
     c = conn.cursor()
     c.execute("SELECT task FROM todo WHERE id LIKE ?", (json,))
     result = c.fetchall()
     c.close()
 
     if not result:
-        return {'task': 'This item number does not exist!'}
+        return {"task": "This item number does not exist!"}
     else:
-        return {'task': result[0]}
+        return {"task": result[0]}
 
 
 @error(403)
 def mistake403(code):
-    return 'There is a mistake in your url!'
+    return "There is a mistake in your url!"
 
 
 @error(404)
 def mistake404(code):
-    return 'Sorry, this page does not exist!'
+    return "Sorry, this page does not exist!"
+
+
 debug(True)
 run(reloader=True)
