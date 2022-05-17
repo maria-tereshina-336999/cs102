f005f17d541e330b68f9f10b7e2384e96350d6f7
Maria Tereshina <90193210+maria-tereshina-336999@users.noreply.github.com>
2022-05-17 23:07:29 +0300

    HackerNews

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,36 +1,96 @@
-from bottle import (
-    route, run, template, request, redirect
-)
+from bottle import error, redirect, request, route, run, template
 
-from scrapper import get_news
-from db import News, session
 from bayes import NaiveBayesClassifier
+from db import News, session
+from scraputils import get_news
 
 
 @route("/news")
 def news_list():
     s = session()
     rows = s.query(News).filter(News.label == None).all()
-    return template('news_template', rows=rows)
+    return template("news_template", rows=rows)
 
 
-@route("/add_label/")
+@route("/add_label/", method="GET")
 def add_label():
-    # PUT YOUR CODE HERE
+    s = session()
+    label = request.GET.get("label", "")
+    id = int(request.GET.get("id", ""))
+    row = s.query(News).filter(News.id == id).one()
+    row.label = label
+    s.add(row)
+    s.commit()
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    new_news = get_news("https://news.ycombinator.com/newest", n_pages=5)
+    s = session()
+    for record in new_news:
+        if (
+            s.query(News)
+            .filter(News.title == record["title"] and News.author == record["author"])
+            .first()
+            is None
+        ):
+            data = News(
+                title=record["title"],
+                author=record["author"],
+                url=record["url"],
+                comments=record["comments"],
+                points=record["points"],
+                label=None,
+            )
+            s.add(data)
+    s.commit()
     redirect("/news")
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    classifier = NaiveBayesClassifier()
+    X_train = [r.title for r in s.query(News).filter(News.label != None).all()][0:300]
+    y_train = [r.label for r in s.query(News).filter(News.label != None).all()][0:300]
+    predict_db = s.query(News).filter(News.label == None).all()
+    X_predict = [r.title for r in predict_db][0:100]
+    classifier.fit(X_train, y_train)
+    labels = classifier.predict(X_predict)
+    for i in range(30):
+        predict_db[i].label = labels[i]
+    classified = [x for x in predict_db if x.label == "good"]
+    classified.extend([x for x in predict_db if x.label == "maybe"])
+    classified.extend([x for x in predict_db if x.label == "never"])
+    return template("predictions_template.tpl", rows=classified)
+
+
+@error(500)
+def mistake500(code):
+    return (
+        "<head>"
+        '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>'
+        "</head>"
+        "<h2>Ошибка на сервере! </h2>"
+        '<th colspan="7">'
+        '<a href="/news" class="ui button">Вернуться на главную страницу</a>'
+        "</th>"
+    )
+
+
+@error(404)
+def mistake404(code):
+    return (
+        "<head>"
+        '<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>'
+        "</head>"
+        "<h2>Этой страницы не существует!</h2>"
+        '<th colspan="7">'
+        '<a href="/news" class="ui button">Вернуться на главную страницу</a>'
+        "</th>"
+    )
 
 
 if __name__ == "__main__":
-    run(host="localhost", port=8080)
-
+    run(host="localhost", port=8080, reloader=True)
--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -3,30 +3,51 @@ from bs4 import BeautifulSoup
 
 
 def extract_news(parser):
-    """ Extract news from a given web page """
-    news_list = []
-
-    # PUT YOUR CODE HERE
-
-    return news_list
+    """Extract news from a given web page"""
+    body = parser.table.findAll("table")[1]
+    titles = [title.text for title in body.findAll("a", {"class": "titlelink"})]
+    urls = [url["href"] for url in body.findAll("a", {"class": "titlelink"})]
+    for i in range(len(urls)):
+        if urls[i][:4] == "item":
+            urls[i] = "https://news.ycombinator.com/" + urls[i]
+    authors = [user.text for user in body.findAll("a", {"class": "hnuser"})]
+    points = [score.text.split()[0] for score in body.findAll("span", {"class": "score"})]
+    ids = [post["id"] for post in body.findAll("tr", {"class": "athing"})]
+    discussions = [
+        body.findAll("span", {"id": f"unv_{id}"})[0].findNext("a", {"href": f"item?id={id}"}).text
+        for id in ids
+    ]
+    comments = [0 if element.isalpha() else int(element.split()[0]) for element in discussions]
+    news = []
+    for i, _ in enumerate(titles):
+        news.append(
+            {
+                "title": titles[i],
+                "url": urls[i],
+                "author": authors[i],
+                "points": points[i],
+                "comments": comments[i],
+            }
+        )
+    return news
 
 
 def extract_next_page(parser):
-    """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    """Extract next page URL"""
+    return parser.findAll("table")[2].findAll("a", {"class": "morelink"})[0]["href"]
 
 
 def get_news(url, n_pages=1):
-    """ Collect news from a given web page """
+    """Collect news from a given web page"""
     news = []
     while n_pages:
         print("Collecting data from page: {}".format(url))
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
         news_list = extract_news(soup)
+
         next_page = extract_next_page(soup)
         url = "https://news.ycombinator.com/" + next_page
         news.extend(news_list)
         n_pages -= 1
     return news
-
