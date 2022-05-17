381d470e09b641f53603ab3bff2e2b00fbd1e341
Maria Tereshina <90193210+maria-tereshina-336999@users.noreply.github.com>
2022-05-17 23:43:14 +0300

    Update db.py

--- homework06/db.py
+++ homework06/db.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, create_engine  # type: ignore
+from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
 
 Base = declarative_base()
 engine = create_engine("sqlite:///news.db")
 session = sessionmaker(bind=engine)
 
 
-class News(Base):
+class News(Base):  # type: ignore
     __tablename__ = "news"
     id = Column(Integer, primary_key=True)
     title = Column(String)
